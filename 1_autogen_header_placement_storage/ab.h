8f73889da3e1cc8067bf9b45ac53c23fa2ffbced
ajneu <ajneu@users.noreply.github.com>
2016-01-09 09:53:13 +0100

    explain that angle-bracket includes, prefer system dirs and dirs given with -I

--- 1_autogen_header_placement_storage/ab.h
+++ 1_autogen_header_placement_storage/ab.h
@@ -1,7 +1,18 @@
 #ifndef AB_H
 #define AB_H
 
-#include <ab_impl_aligninfo.h> // the real version of this header, is generated via cmake (and CMakeLists.txt) in the build directory!
+#include <ab_impl_aligninfo.h> /* The real version of this header, is generated via cmake (and CMakeLists.txt) in the build directory!
+				  
+				  Angle brackets (#include <...>) are needed! Reason:
+				  Searches FIRST:
+				  * in include directories defined with -I and system-directories (e.g. /usr/include)
+				  Only if then NOT YET FOUND:
+				  * it searches in the same directory as the current file
+
+				  We need this behaviour, for it to pick up the generated header, whose location is given with -I./build
+
+				  (In contrast, using quotes (#include "..."), searches first in the same directory as the current file)
+			       */
 
 
 #include "aligned_storer.h"
